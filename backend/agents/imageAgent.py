d25e865cb48626df16fa11f83da73aef0c11a446
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2025-04-08 01:24:42 +0530

    [FEAT] : Added API Route-> getFolderAnalyzed

---
+++ backend/agents/imageAgent.py
@@ -0,0 +1,8 @@
+import os
+from autogen import AssistantAgent, UserProxyAgent
+from PyPDF2 import PdfReader
+from llmConfig import llm_config
+
+
+def analysis_image(path):
+    pass
--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from controllers import analysisFolder as analyze_folder
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/getrecordsstored/{folder_path}")
+def analysisFolder(folder_path):
+    analyze_folder(folder_path)
